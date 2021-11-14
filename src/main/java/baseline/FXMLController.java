fda361bd485a7337eed59940fda195ca9f8d6c0c
John Ashley <johnashleyworkemail@gmail.com>
2021-11-14 17:15:28 -0500

    added some of the functionality

--- src/main/java/baseline/FXMLController.java
+++ src/main/java/baseline/FXMLController.java
@@ -1,12 +1,17 @@
 package baseline;
 
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.File;
 
@@ -43,6 +48,12 @@ public class FXMLController {
     private MenuItem menuSortValueButton;
 
     @FXML
+    private TableColumn<Item, String> nameColumn;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
     private MenuItem nameSearchButton;
 
     @FXML
@@ -52,49 +63,188 @@ public class FXMLController {
     private MenuItem saveButton;
 
     @FXML
+    private TableColumn<Item, String> serialColumn;
+
+    @FXML
+    private TextField serialField;
+
+    @FXML
     private MenuItem serialSearchButton;
 
     @FXML
-    private TableView<?> tableOfItems;
+    private TableView<Item> tableOfItems;
+
+    @FXML
+    private TableColumn<Item, Integer> valueColumn;
+
+    @FXML
+    private TextField valueField;
 
     @FXML
     private Menu viewListButton;
 
     @FXML
+    void initialize()
+    {
+        //set table cell for description
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+
+        //set table cell for
+        valueColumn.setCellValueFactory(new PropertyValueFactory<>("value"));
+
+        //set table cell for  due dates
+        serialColumn.setCellValueFactory(new PropertyValueFactory<>("serialNumber"));
+
+
+    }
+
+    private int index = 0;
+    private final WarningChecker checker = new WarningChecker();
+
+
+    @FXML
     void addItem(ActionEvent event) {
 
         //creates a new inventory item
+        System.out.println("Hi this works");
+        Item item = new Item();
 
-        //does all the checks for the input info
+        String currentName = nameField.getText();
+        String currentSerial = serialField.getText();
 
-        //adds item to list and table
+        //insert check warning for name
+
+        int currentValue;
+        try
+        {
+            currentValue = Integer.parseInt(valueField.getText());
+        }
+        catch(NullPointerException e)
+        {
+            currentValue = 0;
+        }
+
+        //try catch for this
+
+        //insert check warning for value
+
+        if(!checker.checkName(currentName))
+        {
+            System.out.println("Name is wrong.");
+        }
+        else if(!checker.checkSerial(currentSerial) || !checkSameSerial(currentSerial))
+        {
+            System.out.println("Serial is wrong.");
+        }
+        else if(!checker.checkValue(currentValue))
+        {
+            System.out.println("Value is wrong");
+        }
+        else
+        {
+            item.setName(currentName);
+            item.setSerialNumber(currentSerial);
+            item.setValue(currentValue);
+            //adds item to inventory and table
+            System.out.println("Made it to end");
+            tableOfItems.getItems().add(item);
+            Item.Inventory.add(item);
+        }
+
+
+
 
         //clears all item boxes
+        nameField.clear();
+        valueField.clear();
+        serialField.clear();
 
+    }
+
+    public boolean checkSameSerial(String serial)
+    {
+        for(Item i : Item.getInventory())
+        {
+            if(i.getSerialNumber().equals(serial))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     @FXML
     void deleteAll(ActionEvent event) {
 
         //deletes entire inventory and table
+        Item.getInventory().clear();
+        tableOfItems.getItems().clear();
     }
 
     @FXML
     void deleteItem(ActionEvent event) {
 
-        //takes the selected table row
+        //get selected row and get all the items within the list
+        Item selectedItem = tableOfItems.getSelectionModel().getSelectedItem();
+        ObservableList<Item> itemSelected;
+        ObservableList<Item> allItems;
+        allItems = tableOfItems.getItems();
+        itemSelected = tableOfItems.getSelectionModel().getSelectedItems();
 
-        //makes a list of all items and removes selected item from the table and list
+        //remove the item from the display and todolist
+        Item.Inventory.remove(selectedItem);
+        itemSelected.forEach(allItems::remove);
     }
 
     @FXML
     void editItem(ActionEvent event) {
 
         //very similar to add item
+        int idx = tableOfItems.getSelectionModel().getSelectedIndex();
 
-        //creates new inventory item
+        Item item = new Item();
 
-        //does all the checks for correct input info
+        String currentName = nameField.getText();
+        String currentSerial = serialField.getText();
+
+        //insert check warning for name
+
+        int currentValue;
+        try
+        {
+            currentValue = Integer.parseInt(valueField.getText());
+        }
+        catch(NullPointerException e)
+        {
+            currentValue = 0;
+        }
+
+        //try catch for this
+
+        //insert check warning for value
+
+        if(!checker.checkName(currentName))
+        {
+            System.out.println("Name is wrong.");
+        }
+        else if(!checker.checkSerial(currentSerial) || !checkSameSerial(currentSerial))
+        {
+            System.out.println("Serial is wrong.");
+        }
+        else if(!checker.checkValue(currentValue))
+        {
+            System.out.println("Value is wrong");
+        }
+        else
+        {
+            item.setName(currentName);
+            item.setSerialNumber(currentSerial);
+            item.setValue(currentValue);
+            //adds item to inventory and table
+            System.out.println("Made it to end");
+            tableOfItems.getItems().set(idx, item);
+            Item.Inventory.set(idx, item);
+        }
 
 
     }
--- src/main/java/baseline/WarningChecker.java
+++ src/main/java/baseline/WarningChecker.java
@@ -1,31 +1,47 @@
 package baseline;
 
+import java.util.regex.Pattern;
+
 public class WarningChecker {
 
 
 
     public boolean checkName(String name)
     {
-        //will check if the name length is > 1 and < 257
-
-        //returns true if so, false if not to trigger error
-        return true;
+        //check for string length
+        if(name.length() > 1 && name.length() < 256)
+        {
+            return true;
+        }
+        return false;
     }
 
     public boolean checkSerial(String serialNumber)
     {
+        System.out.println("Checking serial");
         //checks if the serial number is in the A-XXX-XXX-XXX format
+            String pattern = ("\\D-\\d\\d\\d-\\d\\d\\d-\\d\\d\\d");
+            if(!Pattern.matches(pattern, serialNumber))
+            {
+                return false;
+            }
+
 
         //for every item in the list, checks if the serial number is the same
+
 
         //if the serial number is correct and not matching, return true
         //else, returns false for an error
         return true;
     }
 
-    public boolean value(int value)
+    public boolean checkValue(int value)
     {
         //checks if the money value is >= 0.
+        if(value < 1)
+        {
+            return false;
+        }
 
         //returns true if so, false if not to trigger error
         return true;
